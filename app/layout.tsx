56e72f89ca6fcfd075504d1298bf34ac108ab2e6
amarnath666 <amarnathdhumal2001@gmail.com>
2025-08-08 11:22:51 +0530

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import { NavbarDemo } from "@/components/ui/NavbarDemo";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +24,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" >
     
-      <body>
+      <body className={`${geistSans.variable} ${geistMono.variable}   antialiased`}
+      >
     
         {children}</body>
     </html>
